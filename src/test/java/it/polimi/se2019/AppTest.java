152d203ce9b04e7ff485f09ee8145e77d38d145a
Abanoub Faltaous <abanoub.faltaous@mail.polimi.com>
2019-04-07 23:04:01 +0200

    Add implementation of the class Deck

--- src/test/java/it/polimi/se2019/AppTest.java
+++ src/test/java/it/polimi/se2019/AppTest.java
@@ -4,14 +4,10 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
+
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+
     @Test
     public void shouldAnswerWithTrue()
     {
