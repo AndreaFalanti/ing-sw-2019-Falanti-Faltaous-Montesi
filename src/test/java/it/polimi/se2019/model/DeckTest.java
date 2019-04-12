3d68a08453e17d21d13ba64126f80be562b6739b
Abanoub Faltaous <abanoub.faltaous@mail.polimi.com>
2019-04-12 16:53:06 +0200

    Add implementation of the DeckTest and the class Deck

--- src/test/java/it/polimi/se2019/model/DeckTest.java
+++ src/test/java/it/polimi/se2019/model/DeckTest.java
@@ -1,26 +1,55 @@
 package it.polimi.se2019.model;
 
 import org.junit.Test;
-
+import java.util.ArrayList;
 import static org.junit.Assert.*;
 
 public class DeckTest {
 
     @Test
-    public void testDrawCard() {
+    public void testConstruction() {
+        ArrayList<String> cards1 = new ArrayList<>();
+        cards1.add("weapon1");
+        cards1.add("weapon2");
+        cards1.add("weapon3");
+        cards1.add("weapon4");
+        cards1.add("weapon5");
+        Deck<String> deck = new Deck<>(cards1);
 
-        fail();
+        assertEquals(deck.getMCards(),cards1);
+        assertTrue(deck.getMCards().containsAll(deck.getMDeck()));
+    }
+
+
+    @Test
+    public void testDrawCard() {
+        ArrayList<String> cards1 = new ArrayList<>();
+        cards1.add("weapon1");
+        cards1.add("weapon2");
+        cards1.add("weapon3");
+        cards1.add("weapon4");
+        cards1.add("weapon5");
+        Deck<String> deck = new Deck<>(cards1);
+
+        String drawnCard = deck.drawCard();
+        assertEquals("weapon5" , drawnCard);
+        assertEquals(4,deck.getMDeck().size());
     }
 
     @Test
     public void testShuffle() {
+        ArrayList<String> cards1 = new ArrayList<>();
+        cards1.add("weapon1");
+        cards1.add("weapon2");
+        cards1.add("weapon3");
+        cards1.add("weapon4");
+        cards1.add("weapon5");
+        Deck<String> deck = new Deck<>(cards1);
+        ArrayList<String> deck1 = deck.getMDeck();
 
-        fail();
+        deck.shuffle();
+        assertTrue(deck.getMCards().containsAll(deck.getMDeck()));
+        assertNotEquals(deck.getMDeck(),deck1);
     }
 
-    @Test
-    public void testRegenerate() {
-
-        fail();
-    }
 }
