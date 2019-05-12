d4499f46b6008a54c9037b9629fd69b6f27126ca
Abanoub Faltaous <abanoub.faltaous@mail.polimi.com>
2019-05-12 14:57:14 +0200

    Fix a code smell in ClientSocket ,fix method of Deck class and add new method in Deck class

--- src/test/java/it/polimi/se2019/model/DeckTest.java
+++ src/test/java/it/polimi/se2019/model/DeckTest.java
@@ -31,14 +31,23 @@ public class DeckTest {
         cards1.add("weapon1");
         cards1.add("weapon2");
         cards1.add("weapon3");
-        cards1.add("weapon4");
-        cards1.add("weapon5");
         Deck<String> deck = new Deck<>(cards1);
-
+        Deck<String> deck1 = new Deck<>(cards1,false);
         String drawnCard1 = deck.getDeck().get(deck.getDeck().size()-1);
         String drawnCard2 = deck.drawCard();
         assertEquals(drawnCard1 , drawnCard2);
-        assertEquals(4,deck.getDeck().size());
+        assertEquals(2,deck.getDeck().size());
+        deck.drawCard();
+        deck.drawCard();
+        if(deck.isReshuffleable())
+            deck.drawCard();
+        assertEquals(2,deck.getDeck().size());
+        deck1.drawCard();
+        deck1.drawCard();
+        deck1.drawCard();
+        deck1.drawCard();
+        assertEquals(0,deck1.getDeck().size());
+
     }
 
     @Test
