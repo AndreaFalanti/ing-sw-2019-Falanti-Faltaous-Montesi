bc14fc9de7ab2524f4913eb14b7767949a4b7601
Andrea Falanti <a.falanti@gmail.com>
2019-05-28 22:28:29 +0200

    Add GUI controller methods to handle shoot and reload with weapon cards
    It's now possible to click on weapon images after a shoot or reload command to indicate that weapon as target.

--- src/main/java/it/polimi/se2019/view/gui/MainScreen.java
+++ src/main/java/it/polimi/se2019/view/gui/MainScreen.java
@@ -5,6 +5,10 @@ import it.polimi.se2019.model.board.Board;
 import it.polimi.se2019.util.Jsons;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 
 import java.io.IOException;
@@ -14,6 +18,15 @@ public class MainScreen {
     private Pane playerPane;
     @FXML
     private Pane boardPane;
+    @FXML
+    private HBox weaponBox;
+    @FXML
+    private HBox powerUpBox;
+    @FXML
+    private Button undoButton;
+
+    private static final double LOADED_OPACITY = 1;
+    private static final double UNLOADED_OPACITY = 0.4;
 
     private BoardPane mBoardController;
 
@@ -40,6 +53,67 @@ public class MainScreen {
         mBoardController.addTargetDeath(5);
 
         boardPane.getChildren().add(newLoadedPane);
+    }
+
+    public void setWeaponBoxStatus (boolean enable) {
+        if (enable) {
+            weaponBox.setDisable(false);
+            weaponBox.setStyle("-fx-background-color: RED");
+        }
+        else {
+            weaponBox.setDisable(true);
+            weaponBox.setStyle("-fx-background-color: rgba(255, 0, 0, 0.0)");
+        }
+    }
+
+    public void setWeaponLoadStatus (int index, boolean value) {
+        ImageView weaponView = (ImageView)weaponBox.getChildren().get(index);
+        if (value) {
+            weaponView.setOpacity(1);
+        }
+        else {
+            weaponView.setOpacity(0.4);
+        }
+    }
+
+    public void setShootOnWeapon () {
+        setWeaponBoxStatus(true);
+        for (int i = 0; i < weaponBox.getChildren().size(); i++) {
+            setShootingBehaviourOnWeapon(weaponBox.getChildren().get(i), i);
+        }
+    }
+
+    public void setReloadOnWeapon () {
+        setWeaponBoxStatus(true);
+        for (int i = 0; i < weaponBox.getChildren().size(); i++) {
+            setReloadBehaviourOnWeapon(weaponBox.getChildren().get(i), i);
+        }
+    }
+
+    private void setShootingBehaviourOnWeapon (Node weapon, int index) {
+        weapon.setOnMouseClicked(event -> {
+            if (weapon.getOpacity() == LOADED_OPACITY) {
+                System.out.println("Shooting with weapon of index: " + index);
+                setWeaponLoadStatus(index, false);
+                setWeaponBoxStatus(false);
+            }
+            else {
+                System.out.println("Can't shoot with unloaded weapon");
+            }
+        });
+    }
+
+    private void setReloadBehaviourOnWeapon (Node weapon, int index) {
+        weapon.setOnMouseClicked(event -> {
+            if (weapon.getOpacity() == UNLOADED_OPACITY) {
+                System.out.println("Reload weapon of index: " + index);
+                setWeaponLoadStatus(index, true);
+                setWeaponBoxStatus(false);
+            }
+            else {
+                System.out.println("Can't reload an already loaded weapon");
+            }
+        });
     }
 
     public void activateButtonGrid () {
