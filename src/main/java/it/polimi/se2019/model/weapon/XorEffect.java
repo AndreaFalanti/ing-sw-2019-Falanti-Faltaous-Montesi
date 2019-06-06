7af2d047920c819d17f926ddd1ddc1a9cf87108e
Stefano Montesi <dincio.montesi@gmail.com>
2019-06-06 10:40:11 +0200

    Fix merge error

--- src/main/java/it/polimi/se2019/model/weapon/XorEffect.java
+++ src/main/java/it/polimi/se2019/model/weapon/XorEffect.java
@@ -5,10 +5,7 @@ import it.polimi.se2019.view.View;
 public class XorEffect extends Expression {
     @Override
     public Expression eval(ShootContext context) {
-<<<<<<< HEAD
         // TODO: implement this when things are decided
-=======
->>>>>>> Implement PickExpression
         return null;
     }
 }
