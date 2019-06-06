fa99f71019ad04bbdb4a4dae3fc1390c265a4971
Abanoub Faltaous <abanoub.faltaous@mail.polimi.com>
2019-06-06 11:21:18 +0200

    Fix errors in CLIView CLIPlayer in cli package

--- src/main/java/it/polimi/se2019/view/cli/CLIInfo.java
+++ src/main/java/it/polimi/se2019/view/cli/CLIInfo.java
@@ -2,17 +2,15 @@ package it.polimi.se2019.view.cli;
 
 import it.polimi.se2019.model.Player;
 import it.polimi.se2019.model.PlayerColor;
-import it.polimi.se2019.model.Position;
-import it.polimi.se2019.model.weapon.Weapon;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class CLIInfo {
     private List<Player> mPlayers;
-    private PlayerColor mActivePlayer;
+    private String mActivePlayer;
     private Player mOwner;
-    private PlayerColor mOwnerColor;
+    private String mOwnerColor;
+
 
     private final static int sizePlayer = 11;
     private CLIPlayer mPlayer ;
@@ -22,67 +20,21 @@ public class CLIInfo {
         mPlayers = players;
 
         mOwner = owner;
-        mOwnerColor = ownerColor;
-        mActivePlayer = activePlayer;
+
+
     }
 
     public CLIInfo(){
 
     }
 
+    public void initialization(List<Player> players){
+        mPlayer = new CLIPlayer(players);
+    }
     public void setActivePlayer(PlayerColor color){
-        mActivePlayerf = color.getPascalName();
+        mActivePlayer = color.getPascalName();
     }
 
-    public void initializationAllPlayers(List<Player> players){
-        for(Player player: players) {
-            mPlayer[mPlayerName] = player.getName();
-            mPlayer[mPlayerColor] = player.getColor().getPascalName();
-            mPlayer[mPlayerScore] = String.valueOf(player.getScore());
-
-            isDead = String.valueOf(player.isDead());
-            isOverkilled = String.valueOf(player.isOverkilled());
-            setWeaponOtherPlayer(player.getColor(),player.getWeapons());
-            setPosition(player.getColor(),player.getPos());
-        }
-    }
-
-    public void setWeaponOtherPlayer(PlayerColor color, Weapon[] weapons){//to complete with color
-        List<String> nameWeapon = new ArrayList<>();
-        String load;
-        if (weapons == null)
-            mPlayerWeapons = "not have weapon";
-        else{
-            for(Weapon weapon: weapons){
-                if(weapon.isLoaded())
-                    nameWeapon.add(weapon.getName());
-            }
-            mPlayerWeapons = String.join(",",nameWeapon);
-        }
-    }
-
-
-    public void setPosition(PlayerColor color, Position pos){//to complete with color
-        List<String> positionPlayer= new ArrayList<>();
-        if (pos==null)
-            mPlayerPos = "not respawned";
-        else {
-            positionPlayer.add(String.valueOf(pos.getX()));
-            positionPlayer.add(String.valueOf(pos.getY()));
-            mPlayerPos = String.join(",",positionPlayer);
-        }
-    }
-
-    public void setMarks(Player player){
-        //todo
-    }
-
-    public void setDamageTaken(PlayerColor damagedPlayer,int damage, PlayerColor attackingColor){
-        List<String> damageTaken = new ArrayList<>();
-        for(PlayerColor color: damagedPlayer)
-
-
-    }
 
 
 
@@ -94,8 +46,8 @@ public class CLIInfo {
                     target = player;
          return target;
     }
-    public String getDamageTaken(){return mDamageTaken};
-    public PlayerColor getActivePlayer(){return mActivePlayer;}
+
+    public String getActivePlayer(){return mActivePlayer;}
     public Player getOwner(){return mOwner;}
-    public PlayerColor getOwnerColor(){return mOwnerColor;}
+    public String getOwnerColor(){return mOwnerColor;}
 }
--- src/main/java/it/polimi/se2019/view/cli/CLIPlayer.java
+++ src/main/java/it/polimi/se2019/view/cli/CLIPlayer.java
@@ -1,5 +1,13 @@
 package it.polimi.se2019.view.cli;
 
+import it.polimi.se2019.model.Player;
+import it.polimi.se2019.model.PlayerColor;
+import it.polimi.se2019.model.Position;
+import it.polimi.se2019.model.weapon.Weapon;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CLIPlayer {
 
     private String mOwnerName;
@@ -17,5 +25,53 @@ public class CLIPlayer {
     private String mPlayerPowerUps;
     private String mPlayerAmmo;
 
+    public CLIPlayer(List<Player> players){
+        for(Player player: players) {
+            mPlayerName = player.getName();
+            mPlayerColor = player.getColor().getPascalName();
+            mPlayerScore = String.valueOf(player.getScore());
 
-}
+            isDead = String.valueOf(player.isDead());
+            isOverkilled = String.valueOf(player.isOverkilled());
+            setWeaponOtherPlayer(player.getColor(),player.getWeapons());
+            setPosition(player.getColor(),player.getPos());
+        }
+    }
+
+    public void setWeaponOtherPlayer(PlayerColor color, Weapon[] weapons){//to complete with color
+        List<String> nameWeapon = new ArrayList<>();
+        String load;
+        if (weapons == null)
+            mPlayerWeapons = "not have weapon";
+        else{
+            for(Weapon weapon: weapons){
+                if(weapon.isLoaded())
+                    nameWeapon.add(weapon.getName());
+            }
+            mPlayerWeapons = String.join(",",nameWeapon);
+        }
+    }
+
+
+    public void setPosition(PlayerColor color, Position pos){//to complete with color
+        List<String> positionPlayer= new ArrayList<>();
+        if (pos==null)
+            mPlayerPos = "not respawned";
+        else {
+            positionPlayer.add(String.valueOf(pos.getX()));
+            positionPlayer.add(String.valueOf(pos.getY()));
+            mPlayerPos = String.join(",",positionPlayer);
+        }
+    }
+
+    public void setMarks(Player player){
+        //todo
+    }
+
+    public void setDamageTaken(PlayerColor damagedPlayer,int damage, PlayerColor attackingColor){
+        List<String> damageTaken = new ArrayList<>();
+       //todo
+    }
+
+
+    }
