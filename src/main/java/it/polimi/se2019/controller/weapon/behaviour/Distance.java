754aa9cb57c468ac8ed18978e4822e625e4a0636
Stefano Montesi <dincio.montesi@gmail.com>
2019-06-10 11:02:52 +0200

    Add rocket launcher json and implement needed expressions

--- src/main/java/it/polimi/se2019/controller/weapon/behaviour/Distance.java
+++ src/main/java/it/polimi/se2019/controller/weapon/behaviour/Distance.java
@@ -5,9 +5,13 @@ import it.polimi.se2019.controller.weapon.ShootContext;
 
 public class Distance extends Behaviour {
     public Distance() {
-
+        putSub("origin", new You());
     }
 
+    public Distance(Expression origin, Expression amount) {
+        putSub("origin", origin);
+        putSub("amount", amount);
+    }
     public Distance(Expression amount) {
         putSub("amount", amount);
     }
@@ -15,6 +19,7 @@ public class Distance extends Behaviour {
     @Override
     protected Expression continueEval(ShootContext context) {
         Expression moreGeneric = new DistanceRange(
+                getSub("origin"),
                 getSub("amount"),
                 getSub("amount")
         );
