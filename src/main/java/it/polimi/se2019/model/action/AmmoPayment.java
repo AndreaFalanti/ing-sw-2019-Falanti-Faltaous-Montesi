9c5c97c3287f9921fe4cc89636d14ae8fd17936b
Andrea Falanti <a.falanti@gmail.com>
2019-06-25 14:17:45 +0200

    Add new method and javaDoc comments to AmmoPayment
    Method added is used for checking that player can pay a remaining cost with his powerUps in hand.

--- src/main/java/it/polimi/se2019/model/action/AmmoPayment.java
+++ src/main/java/it/polimi/se2019/model/action/AmmoPayment.java
@@ -2,11 +2,19 @@ package it.polimi.se2019.model.action;
 
 import it.polimi.se2019.model.AmmoValue;
 import it.polimi.se2019.model.Player;
+import it.polimi.se2019.model.PowerUpCard;
 
 public final class AmmoPayment {
+    // private constructor, avoids class instantiation
     private AmmoPayment() {
     }
 
+    /**
+     * Handle ammo payment interaction
+     * @param player Player that is paying an ammo cost
+     * @param cost Total cost to pay
+     * @param discardedCards Array of discarded powerUps
+     */
     public static void payCost (Player player, AmmoValue cost, boolean[] discardedCards) {
         if (player == null || cost == null || discardedCards == null) {
             throw new IllegalArgumentException("null values in method payCost");
@@ -16,6 +24,13 @@ public final class AmmoPayment {
         player.payAmmo(cost);
     }
 
+    /**
+     * Check that ammo payment is valid
+     * @param player Player that need to pay a cost
+     * @param cost Total cost to pay
+     * @param discardedCards Array of discarded powerUps
+     * @return true if can pay, false otherwise
+     */
     public static boolean isValid (Player player, AmmoValue cost, boolean[] discardedCards) {
         if (player == null || cost == null || discardedCards == null) {
             throw new IllegalArgumentException("null values in method getErrorResponse");
@@ -45,7 +60,7 @@ public final class AmmoPayment {
     }
 
     /**
-     * Get total ammo considering also power up card discarded
+     * Get total ammo considering also power up cards discarded
      * @param player Player that is performing the reload
      * @param discardedCards Boolean array that indicate discarded cards
      * @return Total ammo with power up bonus ones
@@ -61,6 +76,11 @@ public final class AmmoPayment {
         return ammo;
     }
 
+    /**
+     * Discard selected powerUps and give ammo to player
+     * @param player Player that discard powerUps
+     * @param discardedCards Array of discarded powerUps
+     */
     public static void addAmmoAndDiscard (Player player, boolean[] discardedCards) {
         if (player == null || discardedCards == null) {
             throw new IllegalArgumentException("null values in method addAmmoAndDiscard");
@@ -73,5 +93,25 @@ public final class AmmoPayment {
                 player.discard(i);
             }
         }
+    }
+
+    /**
+     * Check if player can pay an additional ammo cost not covered by his actual ammo, discarding the
+     * powerUps in his hand.
+     * @param player Player that pay the cost
+     * @param remainingCost Remaining cost to pay
+     * @return True if can pay, false otherwise
+     */
+    public static boolean canPayWithPowerUps (Player player, AmmoValue remainingCost) {
+        AmmoValue ammoValue = new AmmoValue(0,0,0);
+        PowerUpCard[] powerUpCards = player.getPowerUps();
+
+        for (int i = 0; i < powerUpCards.length; i++) {
+            if (powerUpCards[i] != null) {
+                ammoValue.add(powerUpCards[i].getAmmoValue());
+            }
+        }
+
+        return ammoValue.isBiggerOrEqual(remainingCost);
     }
 }
